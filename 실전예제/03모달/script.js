e30d668788ea6748eebd72e131f0b3329fee1251
dahaeee <33ekgo@naver.com>
2022-06-08 22:32:39 +0900

    no message

--- 실전예제/03모달/script.js
+++ 실전예제/03모달/script.js
@@ -1,7 +1,27 @@
+// fade in out
+// $('.modal-notice').click(function(){
+//     $('.modal').fadeIn();
+// })
+// $('.btn-close').click(function(){
+//     $('.modal').fadeOut();
+// })
+
+// animation
 $('.modal-notice').click(function(){
-    $('.modal').fadeIn();
+    $('.modal').addClass('active');
+})
+$('.btn-close').click(function(){
+    $('.modal').removeClass('active');
 })
 
-$('.btn-close').click(function(){
-    $('.modal').fadeOut();
-})
+// 바닐라 JS
+// let modal = document.getElementById('modal-notice');
+//-> modal에 id 넣어주면됨
+
+// function openModal() {
+//     modal.classList.add('active');
+// }
+
+// function closeModal() {
+//     modal.classList.remove('active');
+// }
